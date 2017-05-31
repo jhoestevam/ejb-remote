0172cf683335d66020266da74e8e31b83badcc45
Jhonatan Martins Estevam <jhonatanm222@gmail.com>
2017-05-31 20:25:35 -0300

    ejb-remote web removido

--- aluno-server/src/main/webapp/scripts/services/AlunoFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('aluno-server').factory('AlunoResource', function($resource){
-    var resource = $resource('rest/alunos/:AlunoId',{AlunoId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});
